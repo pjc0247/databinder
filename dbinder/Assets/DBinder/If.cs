3dbd11d1f8c5edbb1f51afa456497e7d8858ebe9
Park June Chul <pjc0247@naver.com>
2020-01-06 17:05:47 +0900

    update

---
+++ dbinder/Assets/DBinder/If.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If : Bind
+{
+    public override void Invalidate()
+    {
+        var obj = GetValue();
+
+        if (obj.IsTrue())
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+    }
+}
